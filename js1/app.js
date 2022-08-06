610334e3ebb35caab05cb46c34ace7c8010c4c66
nese <neseispir2@gmail.com>
2022-08-06 22:52:23 +0300

    javascript

---
+++ js1/app.js
@@ -0,0 +1,61 @@
+console.log('app.js is loaded');
+
+// let firstName = prompt('Enter your name:');
+// alert('firstName :' + firstName);
+
+// let sonuc = confirm('are you legal?');
+// console.log(sonuc);
+
+// var myNumber1 = 11;
+// console.log(myNumber1);
+// let v1 = 55;
+// console.log(v1);
+/*
+some functions
+
+
+
+
+
+*/
+
+// var myNumber1 = 'mergaba';
+// console.log(myNumber1);
+// v2 = 88;
+// console.log(v2);
+
+// ! const mevzuu
+
+const student = {
+  name: 'Nedim',
+  age: 34,
+};
+
+console.log(student);
+
+const otherStudent = student;
+otherStudent.name = 'Dan Brown';
+
+student = {
+  name: 'Ryan',
+  age: 44,
+};
+
+console.log(student);
+console.log(otherStudent);
+
+// ? primitive örnek
+
+let num1 = 501;
+console.log('num1 :>> ', num1);
+
+num1 = 888;
+console.log('num1 :>> ', num1);
+
+let num2 = 467;
+
+num1 = num2;
+
+// num2 = num2 + 3;
+
+console.log(num1, num2);
